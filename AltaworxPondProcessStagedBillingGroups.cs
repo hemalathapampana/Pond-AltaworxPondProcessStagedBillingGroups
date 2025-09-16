0eb033222546e960814c7837cea94f4bca3e3694
hemalathapampana <137771820+hemalathapampana@users.noreply.github.com>
2025-09-16 20:35:30 +0530

    Add files via upload

---
+++ AltaworxPondProcessStagedBillingGroups.cs
@@ -0,0 +1,118 @@
+﻿using Altaworx.AWS.Core;
+using Altaworx.AWS.Core.Models;
+using Altaworx.AWS.Core.Services.SQS;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using Amop.Core.Constants;
+using Amop.Core.Helpers.Pond;
+using Amop.Core.Models;
+using Amop.Core.Repositories;
+using Amop.Core.Repositories.Environment;
+using Amop.Core.Repositories.Pond;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AltaworxPondProcessStagedBillingGroups;
+
+public class Function : AwsFunctionBase
+{
+    private string GetDevicesQueueURL = string.Empty;
+    public PondRepository pondRepository;
+    public ServiceProviderRepository serviceProviderRepository;
+    protected SqsService sqsService = new SqsService();
+    private readonly EnvironmentRepository environmentRepo = new EnvironmentRepository();
+
+    public async Task FunctionHandler(SQSEvent sqsEvent, ILambdaContext context)
+    {
+        AmopLambdaContext? lambdaContext = null;
+        try
+        {
+            lambdaContext = BaseAmopFunctionHandler(context);
+            ArgumentNullException.ThrowIfNull(lambdaContext);
+
+            InitializeRepositories(lambdaContext);
+
+            TryGetAllEnvironmentVariables(lambdaContext);
+
+            await ProcessEventAsync(lambdaContext, sqsEvent);
+        }
+        catch (Exception ex)
+        {
+            if (lambdaContext == null)
+            {
+                context.Logger.Log(CommonConstants.EXCEPTION, ex.Message + " " + ex.StackTrace);
+            }
+            else
+            {
+                LogInfo(lambdaContext, CommonConstants.EXCEPTION, ex.Message + " " + ex.StackTrace);
+            }
+        }
+
+        base.CleanUp(lambdaContext);
+    }
+
+    protected virtual void InitializeRepositories(AmopLambdaContext lambdaContext)
+    {
+        pondRepository = new PondRepository(lambdaContext.CentralDbConnectionString);
+        serviceProviderRepository = new ServiceProviderRepository(lambdaContext.CentralDbConnectionString);
+    }
+
+    protected void TryGetAllEnvironmentVariables(AmopLambdaContext lambdaContext)
+    {
+        // Lambda related configurations
+        GetDevicesQueueURL = GetStringValueFromEnvironmentVariable(lambdaContext.Context, environmentRepo, PondHelper.CommonString.POND_GET_DEVICES_QUEUE_URL_VARIABLE_KEY);
+    }
+
+    protected SqsValues GetMessageValues(AmopLambdaContext context, SQSEvent.SQSMessage message)
+    {
+        return new SqsValues(context, message);
+    }
+
+    protected async Task ProcessEventAsync(AmopLambdaContext context, SQSEvent sqsEvent)
+    {
+        LogInfo(context, CommonConstants.SUB);
+        if (sqsEvent?.Records != null)
+        {
+            var processedRecordCount = sqsEvent.Records.Count;
+            LogInfo(context, CommonConstants.INFO, string.Format(LogCommonStrings.BEGINNING_PROCESS, processedRecordCount));
+            foreach (var record in sqsEvent.Records)
+            {
+                LogInfo(context, CommonConstants.INFO, $"MessageId: {record.MessageId}");
+                var sqsValues = GetMessageValues(context, record);
+                await ProcessSyncBillingGroupsPageStatus(context, sqsValues);
+            }
+        }
+    }
+
+    protected async Task ProcessSyncBillingGroupsPageStatus(AmopLambdaContext context, SqsValues sqsValues)
+    {
+        // Update the page status
+        // Also in same stored procedure, get the total page count that are still processing
+        // This is to check the sync progress of all service providers
+        var remainingPagesCount = pondRepository.UpdateBillingGroupsPageStatusAndCheckSyncProgress(ParameterizedLog(context), sqsValues.ServiceProviderId, sqsValues.InventoryId, sqsValues.PageNumber, sqsValues.IsSuccessful);
+        // If all done, merge data to main table
+        // Then send message to trigger sync billing groups
+        if (remainingPagesCount == 0)
+        {
+            LogInfo(context, CommonConstants.INFO, string.Format(LogCommonStrings.ALL_PAGES_OF_STEP_HAVE_BEEN_STAGED, PondHelper.CommonString.POND_BILLING_GROUPS_STEP_NAME, DatabaseTableNames.POND_BILLING_GROUP));
+
+            var serviceProviderIds = serviceProviderRepository.GetAllServiceProviderIds(ParameterizedLog(context), IntegrationType.Pond);
+            foreach (var serviceProviderId in serviceProviderIds)
+            {
+                pondRepository.LoadBillingGroupFromStagingTable(ParameterizedLog(context), serviceProviderId, context.Context.FunctionName);
+            }
+            await InitializePondDeviceSyncProcess(context);
+        }
+        else
+        {
+            LogInfo(context, CommonConstants.INFO, string.Format(LogCommonStrings.REMAINING_PAGES_TO_PROCESS, remainingPagesCount, PondHelper.CommonString.POND_BILLING_GROUPS_STEP_NAME));
+        }
+    }
+
+    protected async Task InitializePondDeviceSyncProcess(AmopLambdaContext context)
+    {
+        await sqsService.SendSQSMessage(ParameterizedLog(context), AwsCredentials(context), GetDevicesQueueURL);
+    }
+}
---
+++ ServiceProviderRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Amop.Core.Constants;
+using Amop.Core.Helpers;
+using Amop.Core.Logger;
+using Amop.Core.Models;
+using Amop.Core.Resilience;
+using Microsoft.Data.SqlClient;
+using Polly;
+
+namespace Amop.Core.Repositories
+{
+    public class ServiceProviderRepository : IServiceProviderRepository
+    {
+        private const int MaxRetries = 3;
+        private readonly string connectionString;
+        private readonly ISyncPolicy sqlRetryPolicy;
+
+        public ServiceProviderRepository(string connectionString)
+            : this(connectionString, new NoOpLogger())
+        {
+        }
+
+        public ServiceProviderRepository(string connectionString, IKeysysLogger logger)
+            : this(connectionString, new PolicyFactory(logger))
+        {
+        }
+
+        public ServiceProviderRepository(string connectionString, IPolicyFactory policyFactory)
+            : this(connectionString, policyFactory.GetSqlRetryPolicy(MaxRetries))
+        {
+        }
+
+        public ServiceProviderRepository(string connectionString, ISyncPolicy sqlRetryPolicy)
+        {
+            this.connectionString = connectionString;
+            this.sqlRetryPolicy = sqlRetryPolicy;
+        }
+
+        public ICollection<ServiceProvider> GetEbondingServiceProviders()
+        {
+            return sqlRetryPolicy.Execute(() =>
+            {
+                var serviceProviders = new List<ServiceProvider>();
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    using (var command = new SqlCommand("usp_eBonding_Get_Active_ServiceProviders", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        connection.Open();
+
+                        var rdr = command.ExecuteReader();
+                        while (rdr.Read())
+                        {
+                            serviceProviders.Add(new ServiceProvider { Id = (int)rdr["ServiceProviderId"], TenantId = (int)rdr["TenantId"] });
+                        }
+                    }
+                }
+
+                return serviceProviders;
+            });
+        }
+
+        public virtual List<int> GetAllServiceProviderIds(Action<string, string> logFunction, IntegrationType integrationType)
+        {
+            var parameters = new List<SqlParameter>()
+            {
+                new SqlParameter(CommonSQLParameterNames.INTEGRATION_ID, (int)integrationType),
+            };
+            var serviceProviderIds = sqlRetryPolicy.Execute(() =>
+                SqlQueryHelper.ExecuteStoredProcedureWithListResult(logFunction, connectionString,
+                    SQLConstant.StoredProcedureName.GET_ALL_SERVICE_PROVIDER_IDS_BY_INTEGRATION,
+                    (dataReader) => ReadServiceProviderId(dataReader),
+                    parameters,
+                    SQLConstant.ShortTimeoutSeconds));
+            return serviceProviderIds;
+        }
+
+        private int ReadServiceProviderId(SqlDataReader dataReader)
+        {
+            var columns = dataReader.GetColumnsFromReader();
+            return dataReader.IntFromReader(columns, CommonColumnNames.Id);
+        }
+    }
+}
